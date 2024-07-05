cc493030558c2033623ec9baad4271b8a6600e66
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-07-05 14:31:31 +0100

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Buhecha, Neeta (Trainee Engineer) on 05/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
